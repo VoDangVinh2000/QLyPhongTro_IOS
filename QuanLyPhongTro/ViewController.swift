d05b490a66ad4acc99237be75095397f3900643a
VoDangVinh <01686228222vinh@gmail.com>
2021-05-30 10:19:43 +0700

    update

---
+++ QuanLyPhongTro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuanLyPhongTro
+//
+//  Created by VoDangVinh on 5/28/21.
+//  Copyright © 2021 VoDangVinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
