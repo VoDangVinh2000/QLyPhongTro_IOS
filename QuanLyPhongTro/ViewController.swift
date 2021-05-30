af2eaadd551224847ac09402e8016965b793250b
VoDangVinh <01686228222vinh@gmail.com>
2021-05-30 11:48:59 +0700

    update-

--- QuanLyPhongTro/ViewController.swift
+++ QuanLyPhongTro/ViewController.swift
@@ -14,12 +14,34 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var txtTaiKhoan: UITextField!
+    @IBOutlet weak var txtMatKhau: UITextField!
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func btnLogin(_ sender: Any) {
+        var strTK:String = ""
+        var strMK:String = ""
+        strTK = txtTaiKhoan.text!
+        strMK = txtMatKhau.text!
+
+        if strTK.characters.count == 0{
+            Common.thongBao(parent: self, tieuDe: "Thông báo", noiDung: "Vui lòng nhập tài khoản!")
+            txtTaiKhoan.becomeFirstResponder()
+            return
+        }
+        if strMK.characters.count == 0{
+            Common.thongBao(parent: self, tieuDe: "Thông báo", noiDung: "Vui lòng nhập mật khẩu!")
+            txtMatKhau.becomeFirstResponder()
+            return
+        }
+        if strTK == "admin" && strMK == "admin"{
+            print(strTK + strMK)
+    }
+}
 }
 
